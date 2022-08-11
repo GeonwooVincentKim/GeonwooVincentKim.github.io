40ffaf0b317917c913d566ee912c4fbb3f173ade
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-11 16:18:39 +0900

    [Feature] - Successfully created multi-level Sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,25 +6,43 @@ module.exports = {
             {text: 'About', link: '/about/'},
             {text: 'Contact', link: '/contact/'},
         ],
-        sidebar: [
-            {
-                items: [
-                    {text: 'My Story', link: '/about/my-story'},
-                    // {text: 'About', link: '/about/index'}
-                ],
-
-                '/about/': [
-                    {
-                        text: 'Your Story',
-                        collapsible: true,
-                        collapsed: true,
-                        items: [
-                            {text: 'Your No.1 Story', link: '/about/your/your-story'},
-                            {text: 'Our Story', this: '/about/your/our-story'}
-                        ]
-                    }
-                ]
-            }
-        ],
+        sidebar: {
+            '/about/': [
+                {
+                    text: 'About',
+                    items: [
+                        {text: 'My Story', link: '/about/my-story'},
+                        {text: 'About', link: '/about/index'}
+                    ]
+                }
+            ],
+            '/your/': [
+                {
+                    text: 'Your Story',
+                    items: [
+                        {text: 'Our Story', link: '/your/our-story.md'}, 
+                        {text: 'Your Story', link: '/your/your-story.md'}
+                    ]
+                },
+                {
+                    text: 'Your Story',
+                    items: [
+                        {text: 'Our Story', link: '/your/our-story.md'}, 
+                        {text: 'Your Story', link: '/your/your-story.md'}
+                    ]
+                }
+            ],
+            // items: [
+            //     {text: 'My Story', link: '/about/my-story'},
+            //     {
+            //         text: '',
+            //         items: [
+            //             {text: 'Your No.1 Story', link: '/about/your/your-story'},
+            //             {text: 'Our Story', this: '/about/your/our-story'}
+            //         ]
+            //     }
+            //     // {text: 'About', link: '/about/index'}
+            // ]
+        },
     }
 }
