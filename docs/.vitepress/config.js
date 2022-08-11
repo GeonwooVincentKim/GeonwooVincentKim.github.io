4f09e650ba3b71284045b451136a761d30fe160e
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-11 15:18:50 +0900

    [Feature] - Added config.js and updated .gitignore

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: "Geonwoo Kim's Github-Blog",
+    themeConfig: {
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'About', link: '/about/'},
+            {text: 'Contact', link: '/contact/'},
+        ],
+        sidebar: [
+            {
+                items: [
+                    {text: 'My Story', link: '/about/my-story'},
+                    // {text: 'About', link: '/about/index'}
+                ]
+            }
+        ],
+    }
+}
